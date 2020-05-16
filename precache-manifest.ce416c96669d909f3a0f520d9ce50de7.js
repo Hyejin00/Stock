e6af12de0f4d75203adb4b0fe353a335ff53a249
Hyejin00 <svivs137@naver.com>
2020-05-16 11:48:12 +0900

    Updates

---
+++ precache-manifest.ce416c96669d909f3a0f520d9ce50de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27153219e48f818f8b2470bc85403c8d",
+    "url": "/stock/index.html"
+  },
+  {
+    "revision": "f25a7bdf877dcbad2ffb",
+    "url": "/stock/static/css/main.750ff949.chunk.css"
+  },
+  {
+    "revision": "9e754537e5d68e90324a",
+    "url": "/stock/static/js/2.b665c4eb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/stock/static/js/2.b665c4eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25a7bdf877dcbad2ffb",
+    "url": "/stock/static/js/main.fac2edec.chunk.js"
+  },
+  {
+    "revision": "4a69d63221fce0968a5f",
+    "url": "/stock/static/js/runtime-main.927b3c87.js"
+  }
+]);
